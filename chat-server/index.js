8acc257983b78c7d0cf1e14316edfc57a6496390
Pradeep <pradeep99909@gmail.com>
2020-03-20 22:43:17 +0530

    server files

--- chat-server/index.js
+++ chat-server/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const socket = require("socket.io");
 
+var Auth = require("./auth_function/auth");
 var { Chat } = require("./chat function/chat");
 
 app.post("/get_messages", (req, res) => {
@@ -17,9 +18,53 @@ app.post("/get_messages", (req, res) => {
   res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   var Chat1 = new Chat();
+  //console.log(req.body.uid);
   Chat1.get_new_message(req.body.uid, (response) => {
     res.send(response);
   });
+});
+
+app.post("/chat_history", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  var Chat1 = new Chat();
+  Chat1.chat_history({ uid: req.body.uid, to: req.body.to }, (response) => {
+    res.send(response);
+  });
+});
+
+app.post("/auth_register", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  var Auth1 = new Auth();
+  //console.log(req.body);
+  Auth1.createNewUser(
+    {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    },
+    (response) => {
+      res.send(response);
+    }
+  );
+});
+app.post("/auth_login", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  var Auth1 = new Auth();
+  Auth1.login(
+    {
+      email: req.body.email,
+      password: req.body.password
+    },
+    (response) => {
+      res.status(response.status).send(response);
+    }
+  );
 });
 
 app.post("/send_message", (req, res) => {
@@ -29,6 +74,7 @@ app.post("/send_message", (req, res) => {
   var Chat1 = new Chat();
   Chat1.send_message(
     {
+      from_uid: req.body.uid_from,
       from: req.body.from,
       to: req.body.to,
       message: req.body.message,
