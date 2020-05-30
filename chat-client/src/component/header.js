afbfd7368d7b25d0d709629b7b2bd870773aadd0
Pradeep <pradeep99909@gmail.com>
2020-05-30 17:37:44 +0530

    added options in header

--- chat-client/src/component/header.js
+++ chat-client/src/component/header.js
@@ -1,17 +1,70 @@
 import React from "react";
+import ChatOption from "./chathistory/chatoption";
+import { withRouter } from "react-router";
 
 class ChatHeader extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      display: "none",
+    };
   }
+  display = () => {
+    if (this.state.display === "none") {
+      this.setState((prev) => ({
+        ...prev,
+        display: "block",
+      }));
+    } else {
+      this.setState((prev) => ({
+        ...prev,
+        display: "none",
+      }));
+    }
+  };
   render() {
     return (
       <div className="chat-header">
-        <h1>Chat App</h1>
+        <div className="chat-header-box">
+          <h1>Chat App</h1>
+        </div>
+        <div className="chat-header-box">
+          <i className="material-icons">search</i>
+          <i
+            className="material-icons"
+            onClick={() => {
+              this.props.history.push("/search");
+            }}
+          >
+            person_add
+          </i>
+          <i className="material-icons" onClick={this.display}>
+            more_vert
+          </i>
+        </div>
+        <ChatOption
+          display={this.state.display}
+          top="60px"
+          left="150px"
+          options={[
+            {
+              name: "Settings",
+              //icon: "insert_photo",
+              fun: () => {},
+            },
+            {
+              name: "Logout",
+              //icon: "assignment",
+              fun: () => {
+                localStorage.clear();
+                this.props.history.push("/account/login");
+              },
+            },
+          ]}
+        />
       </div>
     );
   }
 }
 
-export default ChatHeader;
+export default withRouter(ChatHeader);
