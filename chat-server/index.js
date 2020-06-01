e5fa8838963695b76a17b03f5f90d0daabeb7659
Pradeep <pradeep99909@gmail.com>
2020-06-01 14:53:56 +0530

    added port

--- chat-server/index.js
+++ chat-server/index.js
@@ -126,7 +126,7 @@ app.post("/get_message_user", verifyUser, (req, res) => {
     res.send(response);
   });
 });
-const server = app.listen(8000);
+const server = app.listen(8000 || process.env.PORT);
 
 const io = socket(server);
 
